c32efff9a7df57b12dd01b04a2133b95cc320ee6
Light-Lens <73633374+Light-Lens@users.noreply.github.com>
2021-03-15 12:57:18 +0530

    Add Stop button in Leaf

--- Leaf.py
+++ Leaf.py
@@ -50,22 +50,22 @@ class Leaf(QMainWindow):
 		self.Toolbar.setStyleSheet("background-color: #1a1a1a")
 		self.addToolBar(self.Toolbar)
 
-		self.back_btn = QAction(QIcon(os.path.join('res', 'Arrow-Back.png')), "Back", self)
+		self.back_btn = QAction(QIcon(os.path.join('res', 'Arrow-Back.png')), "Go Back One Page", self)
 		self.back_btn.setStatusTip("Go Back One Page")
 		self.back_btn.triggered.connect(self.browser.back)
 		self.Toolbar.addAction(self.back_btn)
 
-		self.forward_btn = QAction(QIcon(os.path.join('res', 'Arrow-Forward.png')), "Forward", self)
+		self.forward_btn = QAction(QIcon(os.path.join('res', 'Arrow-Forward.png')), "Go Forward One Page", self)
 		self.forward_btn.setStatusTip("Go Forward One Page")
 		self.forward_btn.triggered.connect(self.browser.forward)
 		self.Toolbar.addAction(self.forward_btn)
 
-		self.reload_btn = QAction(QIcon(os.path.join('res', 'Reload.png')), "Reload", self)
+		self.reload_btn = QAction(QIcon(os.path.join('res', 'Reload.png')), "Reload Current Page", self)
 		self.reload_btn.setStatusTip("Reload Current Page")
 		self.reload_btn.triggered.connect(self.browser.reload)
 		self.Toolbar.addAction(self.reload_btn)
 
-		self.home_btn = QAction(QIcon(os.path.join('res', 'Home.png')), "Home", self)
+		self.home_btn = QAction(QIcon(os.path.join('res', 'Home.png')), "Leaf Home Page", self)
 		self.home_btn.setStatusTip("Leaf Home Page")
 		self.home_btn.triggered.connect(self.Go_HOME)
 		self.Toolbar.addAction(self.home_btn)
@@ -78,6 +78,14 @@ class Leaf(QMainWindow):
 		self.urlbar.returnPressed.connect(self.navigate_to_url)
 		self.Toolbar.addWidget(self.urlbar)
 		self.urlbar.setFont(self.font)
+
+		self.stop_btn = QAction(QIcon(os.path.join('res', 'Stop.png')), "Stop loading Current Page", self)
+		self.stop_btn.setStatusTip("Stop loading Current Page")
+		self.stop_btn.triggered.connect(self.browser.stop)
+		self.Toolbar.addAction(self.stop_btn)
+
+		self.stop_btn = QAction(QIcon(os.path.join('res', 'Options.png')), "Options menu", self)
+		self.stop_btn.setStatusTip("Options menu")
 
 	# Open Google (Home page for Leaf)
 	def Go_HOME(self): self.browser.setUrl(QUrl("https://www.google.com"))
